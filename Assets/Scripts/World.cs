835c19c016f446b9a87f0c72b5326966dbcfee33
lassewedin <lassewedin@gmail.com>
2017-06-03 21:33:30 +0200

    Make it possible to control speed of process

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -5,16 +5,16 @@ public class World : MonoBehaviour {
 
     public Life life;
 
-    private float time;
+    private float fixedTime;
 
 	void Start () {
 
         //test, OK with 24 * 24 (18 cells per creature) ~ 10 FPS :) 
-        for (int y = 1; y <= 10; y++) {
-            for (int x = 1; x <= 10; x++) {
-                life.SpawnCreatureEmbryo(new Vector3(x*10f, y*10, 0f));
+        for (int y = 1; y <= 14; y++) {
+            for (int x = 1; x <= 14; x++) {
+                life.SpawnCreatureEmbryo(new Vector3(x * 10f, y * 10, 0f));
             }
-         }
+        }
         //life.SpawnCreatureEmbryo(new Vector3(50f, 50f, 0f));
 
         //life.SpawnCreatureEmbryo(new Vector3(10f, 20f, 0f));
@@ -23,10 +23,22 @@ public class World : MonoBehaviour {
     }
 
     void Update() {
-        life.EvoUpdate();
+        //Handle time from here to not get locked out
+        if (HUD.instance.timeControllValue == 0) {
+            Time.timeScale = 0;
+            life.EvoUpdate();
+        } else if (HUD.instance.timeControllValue == 1) {
+            Time.timeScale = 1;
+            life.EvoUpdate();
+        } else {
+            Time.timeScale = 4;
+        } 
     }
 
     void FixedUpdate() {
-        life.EvoFixedUpdate();
+        if (HUD.instance.timeControllValue > 0) {
+            fixedTime += Time.fixedDeltaTime;
+            life.EvoFixedUpdate(fixedTime);
+        }
     }
 }
