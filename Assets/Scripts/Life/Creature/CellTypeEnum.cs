d80354b01a0233739d3e3aed96a552073e861564
lassewedin <lassewedin@gmail.com>
2017-06-09 23:06:45 +0200

    Set graphical gene reference color and index

---
+++ Assets/Scripts/Life/Creature/CellTypeEnum.cs
@@ -0,0 +1,26 @@
+﻿public enum CellTypeEnum {
+    Leaf,
+    Mouth,
+    Muscle,
+    Vein,
+}
+/*
+Void,
+Stem,
+Muscle,
+Leaf,
+Vein,
+Mouth,
+Shell,
+Fat,
+Bone,
+Egg,
+Sperm, //Slurm
+Bomb,
+Eye,
+Nerve,
+Glue,
+Link,
+Fork,
+*/
+
---
+++ Assets/Scripts/Life/Creature/CellTypeUtil.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class CellTypeUtil {
+
+    private static Dictionary<CellTypeEnum, Color> cellTypeToColor = new Dictionary<CellTypeEnum, Color>();
+
+    static CellTypeUtil() {
+        cellTypeToColor.Add(CellTypeEnum.Leaf,      new Color(0f,   1,      0f,     1f));
+        cellTypeToColor.Add(CellTypeEnum.Mouth,     new Color(1f,   0,      1f,     1f));
+        cellTypeToColor.Add(CellTypeEnum.Muscle,    new Color(1f,   0.5f,   0f,     1f));
+        cellTypeToColor.Add(CellTypeEnum.Vein,      new Color(1f,   0f,     0f,     1f));
+    }
+
+    public static Color ToColor(CellTypeEnum cellType) {
+        return cellTypeToColor[cellType];
+    }
+}
