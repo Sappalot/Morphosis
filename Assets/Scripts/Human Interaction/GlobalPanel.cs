a8dc532ea5ddd63924a372b83d5d03104466b942
lassewedin <lassewedin@gmail.com>
2017-08-13 02:54:15 +0200

    Add global panel

---
+++ Assets/Scripts/Human Interaction/GlobalPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalPanel : MonoBehaviour {
+
+    public Text fps;
+
+    private int frameCount;
+    private float timeCount;
+    private float updateTimeCount;
+    private float updatePeriod = 1f;
+
+	void Update () {
+        frameCount++;
+        timeCount += Time.deltaTime;
+        updateTimeCount += Time.deltaTime;
+        if (updateTimeCount > updatePeriod) {
+            updateTimeCount -= updatePeriod;
+            fps.text = string.Format("FPS: {0:F1}", frameCount / timeCount);
+            frameCount = 0;
+            timeCount = 0;
+        }        
+	}
+}
