b76643846de17283a7222862af7b469b2c9cb086
lassewedin <lassewedin@gmail.com>
2017-08-28 23:58:43 +0200

    Forgotten stuff

---
+++ Assets/Scripts/Serialization/CellData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class CellData {
+    public Vector3 position = Vector3.zero;
+    public float heading; //Flip triangle is pointing in this direction at the moment
+    public int bindCardinalIndex;
+    public int geneIndex = 0;
+    public Vector2i mapPosition;
+    public int buildOrderIndex;
+    public FlipSideEnum flipSide;
+    public float radius = 0.5f;
+    public Vector2 velocity;
+    public float timeOffset; //To be removed
+    public float lastTime; //Last time muscle cell was updated
+}
