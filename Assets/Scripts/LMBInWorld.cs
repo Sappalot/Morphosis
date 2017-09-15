10ebb0dccf62936e7fe5c4ef5c76773cab1c75f5
lassewedin <lassewedin@gmail.com>
2017-09-15 19:19:25 +0200

    Add LMB in world

---
+++ Assets/Scripts/LMBInWorld.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LMBInWorld : MonoBehaviour {
+
+	//Bevare of LMB Mouse Drag in selectionController
+	private void OnMouseDown() {
+		if (Input.GetKey("mouse 0") && !EventSystem.current.IsPointerOverGameObject()) {
+			if (MouseAction.instance.actionState == MouseActionStateEnum.free) { 
+				if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.LeftShift)) {
+					return;
+				}
+				CreatureSelectionPanel.instance.ClearSelection();
+			} else if (MouseAction.instance.actionState == MouseActionStateEnum.moveCreatures) {
+				CreatureSelectionPanel.instance.PlaceHoveringCreatures();
+			}
+		}
+	}
+}
