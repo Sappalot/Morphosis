a821223572bf7dfcc631b5272e7e71974a0e4942
lassewedin <lassewedin@gmail.com>
2017-09-30 20:37:24 +0200

    Add Egg panel

---
+++ Assets/Scripts/Human Interaction/Phenotype/CellPanel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CellPanel : MonoSingleton<CellPanel> {
+	public Text cellType;
+	public Text cellEnergy;
+
+	public EggPanel eggPanel;
+
+	private Cell m_cell;
+	public Cell cell {
+		get {
+			return m_cell;
+		}
+		set {
+			m_cell = value;
+			UpdateRepresentation();
+		}
+	}
+
+	public void UpdateRepresentation() {
+		eggPanel.gameObject.SetActive(false);
+
+		//Nothing to represent
+		if (cell == null) {
+			cellType.text = "Type:";
+			cellEnergy.text = "Energy:";
+			return;
+		}
+
+		cellType.text = "Type: " + cell.gene.type.ToString();
+		cellEnergy.text = "Energy: 100%";
+
+		if (cell is EggCell) {
+			eggPanel.gameObject.SetActive(true);
+		}  
+	}
+}
