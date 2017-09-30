7359001003b9b3c562ac67134ad6230d4f5a4bde
lassewedin <lassewedin@gmail.com>
2017-09-30 20:36:58 +0200

    Add Egg cell

---
+++ Assets/Scripts/Life/Creature/Cells/EggCell.cs
@@ -0,0 +1,24 @@
+﻿
+public class EggCell : Cell {
+	public EggCell() : base() {
+		springFrequenzy = 5f;
+		springDamping = 11f;
+	}
+
+	public override void UpdateSpringFrequenzy() {
+		if (HasNeighbour(CardinalEnum.north)) {
+			northSpring.frequency = (this.springFrequenzy + northNeighbour.cell.springFrequenzy) / 2f;
+			northSpring.dampingRatio = (this.springDamping + northNeighbour.cell.springDamping) / 2f;
+		}
+
+		if (HasNeighbour(CardinalEnum.southWest)) {
+			southWestSpring.frequency = (this.springFrequenzy + southWestNeighbour.cell.springFrequenzy) / 2f;
+			southWestSpring.dampingRatio = (this.springDamping + southWestNeighbour.cell.springDamping) / 2f;
+		}
+
+		if (HasNeighbour(CardinalEnum.southEast)) {
+			southEastSpring.frequency = (this.springFrequenzy + southEastNeighbour.cell.springFrequenzy) / 2f;
+			southEastSpring.dampingRatio = (this.springDamping + southEastNeighbour.cell.springDamping) / 2f;
+		}
+	}
+}
