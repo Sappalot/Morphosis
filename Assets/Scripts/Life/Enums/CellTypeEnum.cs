62e8cd86d8e47a74f8c6e6fb09f1a604fdc87d5f
lassewedin <lassewedin@gmail.com>
2017-09-30 20:36:05 +0200

    Moving classes

---
+++ Assets/Scripts/Life/Enums/CellTypeEnum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+[Serializable]
+public enum CellTypeEnum {
+	Egg,
+	Jaw,
+	Leaf,
+	Muscle,
+	Vein,
+}
+/*
+Void,
+Stem,
+Muscle,
+Leaf,
+Vein,
+Mouth,
+Shell,
+Fat,
+Bone,
+
+Sperm, //Slurm
+Bomb,
+Eye,
+Nerve,
+Glue,
+Link,
+Fork,
+*/
+
