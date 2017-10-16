5e7e1f1e86c4b3fa16f725474ea7af6cd6ef525d
lassewedin <lassewedin@gmail.com>
2017-10-16 11:47:48 +0200

    Fix bug where genome graphics got scrambled at auto scroll

--- Assets/Scripts/Human Interaction/Genotype/GenomePanel.cs
+++ Assets/Scripts/Human Interaction/Genotype/GenomePanel.cs
@@ -57,22 +57,24 @@ public class GenomePanel : MonoSingleton<GenomePanel> {
 				genomeGenes[index].gene = genotype.GetGeneAt(index);
 			}
 			isDirty = false;
-		}
-
-		if (isScrollDirty) {
-			if (genotype == null) {
-				isScrollDirty = false;
-				return;
-			}
-
-			// horizontalNormalizedPosition causes faulty update. Fix it!! 
-			for (int index = 0; index < genomeGenes.Length; index++) {
-				if (genomeGenes[index].gene == GenePanel.instance.selectedGene) {
-					scrollRect.horizontalNormalizedPosition = (float)index / ((float)genomeGenes.Length - 1f);
+		} else {
+			// Don't scroll at the same frame that craphics in scroll rect is updated!
+			// Doing so will ocationally cause scrambled graphics
+			if (isScrollDirty) {
+				if (genotype == null) {
+					isScrollDirty = false;
+					return;
 				}
-			}
 
-			isScrollDirty = false;
+				// horizontalNormalizedPosition causes faulty update. Fix it!! 
+				for (int index = 0; index < genomeGenes.Length; index++) {
+					if (genomeGenes[index].gene == GenePanel.instance.selectedGene) {
+						scrollRect.horizontalNormalizedPosition = (float)index / ((float)genomeGenes.Length - 1f);
+					}
+				}
+
+				isScrollDirty = false;
+			}
 		}
 	}
 }
