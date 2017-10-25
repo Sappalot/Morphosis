ef5ef5ee4aff9b5d804cab788ee7550c5a55082c
lassewedin <lassewedin@gmail.com>
2017-10-25 13:52:22 +0200

    Add soul data

---
+++ Assets/Scripts/Serialization/SoulData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class SoulData {
+	public string id = "no id";
+
+	//??
+	public string motherId = string.Empty; //means has no mother
+	public bool isMotherConnected;
+
+	public string[] childrenId;
+	public bool[] isChildrenConnected;
+	public Vector2i[] childrenRootMapPosition; //In mothers frame of reference
+	public int[] rootBindCardinalIndex; //In mothers frame of reference
+}
