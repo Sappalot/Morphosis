3a92c9dd0f12b074d3289d119eae59c7231f94c6
lassewedin <lassewedin@gmail.com>
2017-12-10 08:57:42 +0100

    Add menu popup

---
+++ Assets/Scripts/Human Interaction/MenuPopup.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MenuPopup : MonoBehaviour {
+
+	public RectTransform popupPanel;
+
+	public void OnButtonClick() {
+		popupPanel.gameObject.SetActive(!popupPanel.gameObject.activeSelf);
+	}
+}
