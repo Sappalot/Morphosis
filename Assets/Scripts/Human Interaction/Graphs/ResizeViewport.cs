ff6af78d7cba257a2b9861972c1455b1656524e9
lassewedin <lassewedin@gmail.com>
2018-06-05 00:48:42 +0200

    Make graph view appear in the right position

---
+++ Assets/Scripts/Human Interaction/Graphs/ResizeViewport.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ResizeViewport : MonoBehaviour {
+	public RectTransform windowSize;
+	public Camera viewportToResize;
+
+	public int rightMargin;
+	public int topMargin;
+	public int height;
+
+	private Vector2i res;
+
+	// Use this for initialization
+	void Start() {
+		res = new Vector2i();
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+		if (res.x != (int)windowSize.rect.width || res.y != (int)windowSize.rect.height) {
+
+			float viewportHeight = height / windowSize.rect.height;
+			float viewportWidth = (windowSize.rect.width - rightMargin) / windowSize.rect.width;
+			float viewportY = (windowSize.rect.height - (height + topMargin)) / windowSize.rect.height;
+
+			viewportToResize.rect = new Rect(0, viewportY, viewportWidth, viewportHeight);
+			//Debug.Log("w: " + viewportWidth + ", h: " + viewportHeight + ", y: " + viewportY);
+
+			res = new Vector2i((int)windowSize.rect.width, (int)windowSize.rect.height);
+
+		}
+
+	}
+}
--- Assets/Scripts/Human Interaction/MenuPopup.cs
+++ Assets/Scripts/Human Interaction/MenuPopup.cs
@@ -2,9 +2,15 @@
 
 public class MenuPopup : MonoBehaviour {
 
-	public RectTransform popupPanel;
+	public RectTransform popup1;
+	public Transform popup2;
 
 	public void OnButtonClick() {
-		popupPanel.gameObject.SetActive(!popupPanel.gameObject.activeSelf);
+		if (popup1 != null) {
+			popup1.gameObject.SetActive(!popup1.gameObject.activeSelf);
+		}
+		if (popup2 != null) {
+			popup2.gameObject.SetActive(!popup2.gameObject.activeSelf);
+		}
 	}
 }
