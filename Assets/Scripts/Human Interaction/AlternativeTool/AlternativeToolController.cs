fb5acde50caf79d2e19d0d4ee42bbdca1541fcbd
lassewedin <lassewedin@gmail.com>
2019-05-01 22:20:26 +0200

    Rename Add Alternative tool

---
+++ Assets/Scripts/Human Interaction/AlternativeTool/AlternativeToolController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AlternativeToolController : MouseDrag {
+	public new Camera camera;
+	public SpringJoint2D spring;
+	private Vector3 downPositionMouse; //World space
+
+	public override void OnDraggingStart(int mouseButton) {
+		if (mouseButton == 0 && AlternativeToolModePanel.instance.isOn && !EventSystem.current.IsPointerOverGameObject()) {
+			downPositionMouse = camera.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward * 25;
+			if (AlternativeToolModePanel.instance.toolMode == AlternativeToolModePanel.RMBToolMode.spring && CreatureEditModePanel.instance.mode == PhenoGenoEnum.Phenotype) {
+				Cell cell = World.instance.life.GetCellAtPosition(downPositionMouse);
+				if (cell != null) {
+					spring.connectedBody = cell.theRigidBody;
+					spring.anchor = downPositionMouse;
+					spring.distance = 0f;
+					spring.GetComponent<LineRenderer>().SetPosition(1, downPositionMouse);
+					spring.GetComponent<LineRenderer>().SetPosition(0, spring.connectedBody.transform.position);
+					spring.GetComponent<LineRenderer>().enabled = true;
+				}
+			} else if (AlternativeToolModePanel.instance.toolMode == AlternativeToolModePanel.RMBToolMode.spawnEmbryo && TerrainPerimeter.instance.IsInside(camera.ScreenToWorldPoint(Input.mousePosition))) {
+				if (CreatureEditModePanel.instance.mode == PhenoGenoEnum.Phenotype || CreatureEditModePanel.instance.mode == PhenoGenoEnum.Genotype) {
+					Creature spawned = World.instance.life.SpawnCreatureSimple(downPositionMouse, 90f, World.instance.worldTicks);
+					EffectsUtils.SpawnAddCreature(spawned);
+					HistoryUtil.SpawnAddCreatureEvent(1);
+				}
+			} else if (AlternativeToolModePanel.instance.toolMode == AlternativeToolModePanel.RMBToolMode.spawnFreak && TerrainPerimeter.instance.IsInside(camera.ScreenToWorldPoint(Input.mousePosition))) {
+				if (CreatureEditModePanel.instance.mode == PhenoGenoEnum.Phenotype || CreatureEditModePanel.instance.mode == PhenoGenoEnum.Genotype) {
+					Creature spawned = World.instance.life.SpawnCreatureFreak(downPositionMouse, 90f, World.instance.worldTicks);
+					EffectsUtils.SpawnAddCreature(spawned);
+					HistoryUtil.SpawnAddCreatureEvent(1);
+				}
+			}
+		}
+	}
+
+	public override void OnDragging(int mouseButton) {
+		if (mouseButton == 0) {
+			if (AlternativeToolModePanel.instance.isOn && spring.connectedBody != null) {
+				Vector3 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward * 25;
+				spring.anchor = mousePosition;
+				spring.GetComponent<LineRenderer>().SetPosition(1, mousePosition);
+				spring.GetComponent<LineRenderer>().SetPosition(0, spring.connectedBody.transform.position);
+			}
+
+			if (!AlternativeToolModePanel.instance.isOn) {
+				spring.connectedBody = null;
+				spring.GetComponent<LineRenderer>().enabled = false;
+			}
+		}
+
+	}
+
+	public override void OnDraggingEnd(int mouseButton) {
+		if (mouseButton == 0 && AlternativeToolModePanel.instance.isOn) {
+			spring.connectedBody = null;
+			spring.GetComponent<LineRenderer>().enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Human Interaction/AlternativeTool/AlternativeToolModePanel.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AlternativeToolModePanel : MonoSingleton<AlternativeToolModePanel> {
+	public Image springImage;
+	public Image spawnEmbryoImage;
+	public Image spawnFreakImage;
+
+	public GameObject showHide;
+	public bool isOn;
+
+	public enum RMBToolMode {
+		spring,
+		attract,
+		repell,
+		spawnEmbryo,
+		spawnFreak,
+	}
+
+	private RMBToolMode m_toolMode = RMBToolMode.spawnEmbryo;
+	public RMBToolMode toolMode	{
+		get {
+			return m_toolMode;
+		}
+	}
+
+	public override void Init() {
+		Restart();
+	}
+
+	public void Restart() {
+		m_toolMode = RMBToolMode.spawnEmbryo;
+		UpdateHUD();
+	}
+
+	public void OnClickedSpring() {
+		m_toolMode = RMBToolMode.spring;
+		UpdateHUD();
+	}
+
+	public void OnClickedAttract() {
+		m_toolMode = RMBToolMode.attract;
+		UpdateHUD();
+	}
+
+	public void OnClickedRepell() {
+		m_toolMode = RMBToolMode.repell;
+		UpdateHUD();
+	}
+
+	public void OnClickedSpawnEmbryo() {
+		m_toolMode = RMBToolMode.spawnEmbryo;
+		UpdateHUD();
+	}
+
+	public void OnClickedSpawnFreak() {
+		m_toolMode = RMBToolMode.spawnFreak;
+		UpdateHUD();
+	}
+
+	private void UpdateHUD() {
+		springImage.color = (toolMode == RMBToolMode.spring) ? ColorScheme.instance.selectedButton : ColorScheme.instance.notSelectedButton;
+		spawnEmbryoImage.color = (toolMode == RMBToolMode.spawnEmbryo) ? ColorScheme.instance.selectedButton : ColorScheme.instance.notSelectedButton;
+		spawnFreakImage.color = (toolMode == RMBToolMode.spawnFreak) ? ColorScheme.instance.selectedButton : ColorScheme.instance.notSelectedButton;
+	}
+
+	private void OnGUI() {
+		Event e = Event.current;
+		if (e.alt) {
+			if (!isOn) {
+				showHide.SetActive(true);
+				isOn = true;
+			}
+		} else {
+			if (isOn) {
+				showHide.SetActive(false);
+				isOn = false;
+			}
+		}
+		if (!isFreeToUse()) {
+			if (isOn) {
+				showHide.SetActive(false);
+				isOn = false;
+			}
+		}
+	}
+
+	private bool isFreeToUse() {
+		return MouseAction.instance.actionState == MouseActionStateEnum.free && World.instance.creatureSelectionController.IsIdle;
+	}
+}
