4124706ed581e55d9fa231430de3ee97100b7c54
lassewedin <lassewedin@gmail.com>
2019-05-19 04:37:48 +0200

    Add cellBirth effect

---
+++ Assets/Scripts/Particles/ParticleEffects/ParticlesCellBirth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ParticlesCellBirth : Particles {
+	// 1
+
+	public ParticleSystem cellBirth;
+	public ParticleSystem cover;
+
+	public void Play(Color color) {
+		//cellBirth.startColor = color;
+		cellBirth.Play();
+		cover.Play();
+		StartCoroutine(RemoveSelf()); 
+	}
+
+	private void Update() {
+		if (Time.timeScale < 0.01f) {
+			cellBirth.Simulate(Time.unscaledDeltaTime, true, false);
+			cover.Simulate(Time.unscaledDeltaTime, true, false);
+		}
+	}
+}
