ccb597bde9cc8d41299b8ea3347cca3ca673b9e3
lassewedin <lassewedin@gmail.com>
2019-09-23 23:49:01 +0200

    Add heading to components

--- Assets/Scripts/Human Interaction/Cell And Gene/Common/Sensors/EnergySensorPanel.cs
+++ Assets/Scripts/Human Interaction/Cell And Gene/Common/Sensors/EnergySensorPanel.cs
@@ -22,18 +22,13 @@ public class EnergySensorPanel : SensorPanel {
 				Debug.Log("Update Energy Sensor Panel");
 			}
 
-			if (mode == PhenoGenoEnum.Phenotype) {
-				energyThresholdSlider.interactable = false;
-			} else if (mode == PhenoGenoEnum.Genotype) {
-				if (selectedGene != null) {
-					ignoreSliderMoved = true;
-					energyThresholdSliderLabel.text = string.Format("On if energy > {0:F1} J", (affectedGeneSensor as GeneEnergySensor).threshold);
-					energyThresholdSlider.value = (affectedGeneSensor as GeneEnergySensor).threshold;
-					energyThresholdSlider.interactable = IsUnlocked();
-					ignoreSliderMoved = false;
-				}
+			if (selectedGene != null) {
+				ignoreSliderMoved = true;
+				energyThresholdSliderLabel.text = string.Format("On if energy > {0:F1} J", (affectedGeneSensor as GeneEnergySensor).threshold);
+				energyThresholdSlider.value = (affectedGeneSensor as GeneEnergySensor).threshold;
+				energyThresholdSlider.interactable = IsUnlocked() && mode == PhenoGenoEnum.Genotype;
+				ignoreSliderMoved = false;
 			}
-
 
 			isDirty = false;
 		}
