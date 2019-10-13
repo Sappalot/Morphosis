ae97c9db2f8a2158c7743b26ad7c82d581d3286a
lassewedin <lassewedin@gmail.com>
2019-10-13 23:11:18 +0200

    Make it possible to set embryo max size

---
+++ Assets/Scripts/Life/Creature/Enums/EmbryoMaxSizeModeEnum.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+
+// In between cell Panel and Cell.Signal, to know which output we are trying to update in panel, we must know its name (can't have a reference in a good way)
+[Serializable]
+public enum EmbryoMaxSizeModeEnum {
+	LimitSize,
+	AsBigAsPossible,
+}
