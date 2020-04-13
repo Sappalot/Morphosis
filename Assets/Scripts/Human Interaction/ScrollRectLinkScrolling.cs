7b0352009c9918400177d7f9bf6815d570d6122d
lassewedin <lassewedin@gmail.com>
2020-04-13 05:19:45 +0200

    Make geneCell and Cell panels scroll together :)

---
+++ Assets/Scripts/Human Interaction/ScrollRectLinkScrolling.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollRectLinkScrolling : MonoBehaviour {
+
+	public Transform self;
+	public Transform other;
+
+	public void OnScrolling() {
+		other.localPosition = new Vector3(other.localPosition.x, self.localPosition.y, other.localPosition.z);
+	}
+	
+}
