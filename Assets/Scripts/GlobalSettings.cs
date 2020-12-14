ff90d33753df152869955d0ea49b046e8dde7d85
lassewedin <lassewedin@gmail.com>
2020-12-14 03:48:23 +0100

    Set values in settings to the values that has been tweaked in unity

--- Assets/Scripts/GlobalSettings.cs
+++ Assets/Scripts/GlobalSettings.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class GlobalSettings : MonoSingleton<GlobalSettings> {
 
+	// Coppied all numbers from unity ine here 2020-12-14 Lucia
+
 	public GUISkin popupSkin;
 
 	//To avoid warning "Unable to find style 'MiniToolbarPopup' in skin 'LightSkin' Layout"
@@ -20,7 +22,7 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 
 	[Serializable]
 	public class Mutation {
-		public float masterMutationStrength = 1f;
+		public float masterMutationStrength = 0.25f;
 
 		public float cellTypeChange = 10f;
 
@@ -43,45 +45,45 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 
 		public float axonEnabledToggle = 10f;
 
-		public float axonFromOriginOffsetChange = 50f;
+		public float axonFromOriginOffsetChange = 20f;
 		public float axonFromOriginOffsetChangeMaxAmount = 45f;
 
 		public float axonIsFromOriginPlus180Toggle = 10f;
 
-		public float axonFromMeOffsetChange = 10f;
+		public float axonFromMeOffsetChange = 20f;
 		public float axonFromMeOffsetChangeMaxAmount = 45f;
 
-		public float axonRelaxContractChange = 10f;
+		public float axonRelaxContractChange = 20f;
 		public float axonRelaxContractChangeMaxAmount = 2.5f;
 
 		public float axonIsReverseToggle = 10f;
 
-		public float originEmbryoMaxSizeCompletenessChange = 50f;
-		public float originEmbryoMaxSizeCompletenessChangeMaxAmount = 0.2f; // % of full size 
+		public float originEmbryoMaxSizeCompletenessChange = 20f;
+		public float originEmbryoMaxSizeCompletenessChangeMaxAmount = 0.2f; // part of full size of full size 
 
-		public float originGrowPriorityCellPersistenceChange = 10f;
+		public float originGrowPriorityCellPersistenceChange = 20f;
 		public float originPersistToGrowBlockedPriorityCellPatienseChangeMaxAmount = 30f; //s
 
-		public float originPulseTickPeriodChange = 10f;
+		public float originPulseTickPeriodChange = 20f;
 		public float originPulseTickPeriodChangeMaxAmount = 40f;
 
-		public float buildPriorityBiasChange = 50f;
+		public float buildPriorityBiasChange = 20f;
 		public float buildPriorityBiasChangeMaxAmount = 4f;
 
 		// nerve
-		public float nerveSlotChange = 40f;
+		public float nerveSlotChange = 6f;
 		public float nerveUnitAndSlotChange = 10f;
 
 		// logic box (works the same regardles if inside work panel or not)
-		public float logicBoxInputValveToggle = 10f; // chance per valve
-		public float logicBoxGateExtendFlank = 1f; // extend flank one step left/right. At mutation: for each logicBox -> for each gate -> for each flank -> for each direction... chances are this big that it will be extended 
+		public float logicBoxInputValveToggle = 5f; // chance per valve
+		public float logicBoxGateExtendFlank = 5f; // extend flank one step left/right. At mutation: for each logicBox -> for each gate -> for each flank -> for each direction... chances are this big that it will be extended 
 		public float logicBoxGateRemoveAdd = 5f; // remove presant gate. add a new one size 2 where there is room
 		public float logicBoxGateToggleLogicOperation = 5f;
 
 		// sensor (works the same regardles if inside work panel or not)
-		public float energySensorThresholdChange = 50f;
+		public float energySensorThresholdChange = 20f;
 		public float energySensorThresholdChangeMaxAmount = 30f; // +/-
-		public float energySensorAreaRadiusChange = 50f;
+		public float energySensorAreaRadiusChange = 20f;
 		public float energySensorAreaRadiusChangeMaxAmount = 3f;
 
 		public float effectSensorMeassureChange = 10f;
@@ -91,9 +93,9 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 		public float effectSensorAreaRadiusChange = 50f;
 		public float effectSensorAreaRadiusChangeMaxAmount = 3f;
 
-		public float sizeSensorSizeThresholdChange = 10f;
-		public float sizeSensorSizeThresholdChangeMaxAmount = 0.2f; // % of body size
-		public float sizeSensorCantGrowMorePatienseChange = 50f;
+		public float sizeSensorSizeThresholdChange = 20f;
+		public float sizeSensorSizeThresholdChangeMaxAmount = 0.2f; // part of body size
+		public float sizeSensorCantGrowMorePatienseChange = 20f;
 		public float originGrowPriorityCellPersistenceMaxAmount = 30; // s
 
 		// arrangement
@@ -104,26 +106,26 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 		public float isflipPairsEnabledToggle = 10f;
 		public float typeChange = 10f;
 
-		public float referenceCountDecrease1 = 30f;
-		public float referenceCountDecrease2 = 20f;
-		public float referenceCountDecrease3 = 10f;
-		public float referenceCountIncrease1 = 30f;
-		public float referenceCountIncrease2 = 20f;
-		public float referenceCountIncrease3 = 10f;
+		public float referenceCountDecrease1 = 3f;
+		public float referenceCountDecrease2 = 2f;
+		public float referenceCountDecrease3 = 1f;
+		public float referenceCountIncrease1 = 3f;
+		public float referenceCountIncrease2 = 2f;
+		public float referenceCountIncrease3 = 1f;
 
-		public float arrowIndexDecrease1 = 30f;
-		public float arrowIndexDecrease2 = 20f;
-		public float arrowIndexDecrease3 = 10f;
-		public float arrowIndexIncrease1 = 30f;
-		public float arrowIndexIncrease2 = 20f;
-		public float arrowIndexIncrease3 = 10f;
+		public float arrowIndexDecrease1 = 3f;
+		public float arrowIndexDecrease2 = 2f;
+		public float arrowIndexDecrease3 = 1f;
+		public float arrowIndexIncrease1 = 3f;
+		public float arrowIndexIncrease2 = 2f;
+		public float arrowIndexIncrease3 = 1f;
 
-		public float gapDecrease1 = 30f;
-		public float gapDecrease2 = 20f;
-		public float gapDecrease3 = 10f;
-		public float gapIncrease1 = 30f;
-		public float gapIncrease2 = 20f;
-		public float gapIncrease3 = 10f;
+		public float gapDecrease1 = 3f;
+		public float gapDecrease2 = 2f;
+		public float gapDecrease3 = 1f;
+		public float gapIncrease1 = 3f;
+		public float gapIncrease2 = 2f;
+		public float gapIncrease3 = 1f;
 
 		public float referenceSideToggle = 10f;
 
@@ -151,7 +153,7 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 		[Serializable]
 		public class FungalCell {
 			[Tooltip("The cost of running this cell [W]")]
-			public float effectProductionDown = 0.15f;
+			public float effectProductionDown = 0f;
 			public float transparency = 0.5f;
 		}
 		[Tooltip("Up for grabs")]
@@ -160,32 +162,32 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 		[Serializable]
 		public class JawCell {
 			[Tooltip("The cost of running this cell [W]")]
-			public float effectProductionDown = 0.1f;
+			public float effectProductionDown = 0.08f;
 
 			[Tooltip("The jaws' eating effect [W] depending on my speed [m/s] towards pray cell. Pray cell is losing the same amount, which will show up under External effect")]
 			public AnimationCurve effectProductionUpAtSpeed;
 
 			[Tooltip("How big part of effectProductionUp (the eating effect) that is gaining the cell [W/W] (the rest is wasted into thin air)")]
-			public float effectProductionUpKeepFactor = 0.85f;
+			public float effectProductionUpKeepFactor = 0.8f;
 
-			public float transparency = 0.5f;
+			public float transparency = 0.8f;
 		}
 		public JawCell jawCell;
 
 		[Serializable]
 		public class LeafCell {
 			[Tooltip("The cost of running this cell [W]")]
-			public float effectProductionDown = 5f;
+			public float effectProductionDown = 18f;
 
 			[Tooltip("Ths leafs' fotosyntesis effect which is gaining the cell [W], proportional to the exposure of it.'")]
-			public float effectProductionUpMax = 12f;
+			public float effectProductionUpMax = 32f;
 
 			[Tooltip("A sun beam hitting cell from this far [m] can contribute to its exposure. Or... Cell is searching this far [m] for open space.")]
 			public float sunRayMaxRange = 35.0f;
 
 			//Was 8 and had different for own and opponent cells.
 			[Tooltip("When ray is traveling away from cell it loses this much potential effect (effectProductionUpMax) for each meter it is penetrating cells, measured in [W/m]. This value affects the exposure, which in turn affects the productionUpEffect")]
-			public float sunRayEffectLossPerDistanceThroughCell = 6f;
+			public float sunRayEffectLossPerDistanceThroughCell = 7f;
 
 			[Tooltip("Leaf exposure from a sunRay gives exposure depending on its length (reaching away from the creature). This graph tells how much at what range. X is normalized range, 1 is sunRayMaxRange. Y is exposure at this range, 1 is maximum exposure at 100%")]
 			public AnimationCurve exposureFactorAtRayLengthNormalized;
@@ -200,7 +202,7 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 			public AnimationCurve exposureFactorAtSpeed;
 
 			[Tooltip("How many percentunits, under 0 effect, that we will punish leaf exposure of fast moving creature")]
-			public float exposurePenalty = 0.05f;
+			public float exposurePenalty = 0.1f;
 
 			public float transparency = 0f;
 		}
@@ -209,7 +211,7 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 		[Serializable]
 		public class MuscleCell {
 			[Tooltip("The cost of running this cell [W] This cost is there regardles if we are contracting or not")]
-			public float effectProductionDown = 0.05f;
+			public float effectProductionDown = 0.025f;
 
 			[Tooltip("How much energy [J] we pay for each contraction. From this value an effect cost is calculated which is applied  together with the effectProductionDown when we contract the cell")]
 			public float energyProductionDownPerContraction = 0.05f;
@@ -223,17 +225,17 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 			[Tooltip("The cost of running this cell [W]")]
 			public float effectProductionDown = 0.1f;
 
-			public float transparency = 0.5f;
+			public float transparency = 0.8f;
 		}
 		public RootCell rootCell;
 
 		[Serializable]
 		public class ShellCell {
 			[Tooltip("The cost of running this cell [W]")]
-			public float effectProductionDown = 0.1f;
+			public float effectProductionDown = 0.08f;
 			public float armour = 10f;
-			public float armourAffectFactorOnNeighbour = 0.5f;
-			public float transparency = 0.5f;
+			public float armourAffectFactorOnNeighbour = 0f;
+			public float transparency = 0.8f;
 		}
 		public ShellCell shellCell;
 
@@ -259,10 +261,10 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 		public float finForceSquareFactor = 0.45f;
 
 		[Range(0f, 100f)]
-		public float finForceMax = 1f;
+		public float finForceMax = 0.5f;
 
 		[Range(0f, 1f)]
-		public float nonFinForceFactor = 0.5f;
+		public float nonFinForceFactor = 0.05f;
 
 		//Origin
 		public float originPulseFrequenzyMin = 0.05f; //TODO change to period
@@ -273,9 +275,9 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 		public float buildPriorityBiasMax = 10f;
 
 		//Veins
-		public float veinFluxEffectWeak = 0.05f; //W
-		public float veinFluxEffectMedium = 0.25f; //W
-		public float veinFluxEffectStrong = 0.5f; //W
+		public float veinFluxEffectWeak = 0.015f; //W
+		public float veinFluxEffectMedium = 0.2f; //W
+		public float veinFluxEffectStrong = 0.4f; //W
 
 		//General
 		public int creatureHexagonMaxRadius = 16; // used to limit blueprint. R = 16 ==> Diameter 16 + 1 + 16 ==> we can have can have 16 cells north of origin at most origin = 0, 1 = neighbour, .... cell 16 = perifery 
@@ -286,68 +288,67 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 
 		//General -> build
 		public float cellBuildCost = 10f; //Energy Cost to build cell, J (Newly built cell will have this energy at start of its life)
-		public float cellNewlyBuiltKeepFactor = 1f; //How much of the energy spent on cell that will fill up new cell built
+		public float cellNewlyBuiltKeepFactor = 0.5f; //How much of the energy spent on cell that will fill up new cell built
 		public float cellBuildNeededRadius = 0.35f; //m | a new cell can be built only if there is an empty spot with this radius or more, at the build location (walls are excluded but should be included)
-		public float cellBuildMaxDistance = 1.5f; //m | All neighbours contributing to a new build must be closer than this distance, or new cell can't be built
-		public float cellRebuildCooldown = 40f; //s, before a cell which was killed/deleted can be rebuilt
+		public float cellBuildMaxDistance = 3f; //m | All neighbours contributing to a new build must be closer than this distance, or new cell can't be built
+		public float cellRebuildCooldown = 10f; //s, before a cell which was killed/deleted can be rebuilt
 
 		//General -> Detatch
 		public AnimationCurve detatchmentKickAtCellCount;
-		public float detatchSlideDurationTicks = 10; // s
-		public float detatchSlideDurationTicksRandomDiff = 2; // s to make child locomotion come out of sync with mother
+		public float detatchSlideDurationTicks = 200; // ticks
+		public float detatchSlideDurationTicksRandomDiff = 40; // ticks | to make child locomotion come out of sync with mother
 
 		// General -> Friction (aka drag)
 		public float frictionUnderNormal = 0f;
 		public float frictionUnderNormal1Cell = 0.3f; // used to cancel rotation on twin
 		public float frictionUnderNormal2Cells = 0.1f; // used to slow down single cell
 
-		public float frictionUnderNormalLeaf = 0.5f;
-		public float frictionUnderNormal1CellLeaf = 0.8f; // used to cancel rotation on twin
-		public float frictionUnderNormal2CellsLeaf = 0.6f; // used to slow down single cell
+		public float frictionUnderNormalLeaf = 0f;
+		public float frictionUnderNormal1CellLeaf = 0.3f; // used to cancel rotation on twin
+		public float frictionUnderNormal2CellsLeaf = 0.1f; // used to slow down single cell
 
 		public float frictionUnderSlidingFactor = 0f;
 
 
-		public float springFrequenzy = 5;
-		public float springFrequenzyMuscleCell = 20;
+		public float springFrequenzy = 3;
+		public float springFrequenzyMuscleCell = 5;
 
 		// General -> Teleport
-		public float telepokeImpulseStrength = 1f; // N / teleport tick | impulse applied every teleport tick
+		public float telepokeImpulseStrength = 0.2f; // N / teleport tick | impulse applied every teleport tick
 
 		//Sterile
-		public ulong maxAge = 3600; //s
+		public ulong maxAge = 1800; //s
 
 		//Springs
 		public float springBreakingForce = 100f; // N
-		public float springBreakingForceMuscle = 100f; // N
+		public float springBreakingForceMuscle = 200f; // N
 	}
 	public Phenotype phenotype;
 
 	[Serializable]
 	public class Quality {
 		// Life
-		public int eggCellTickPeriod = 50;
-		public int fungalCellTickPeriod = 50;
-		public int jawCellTickPeriod = 50;
-		public int leafCellTickPeriod = 20;
+		public int eggCellTickPeriod = 20;
+		public int fungalCellTickPeriod = 100;
+		public int jawCellTickPeriod = 5;
+		public int leafCellTickPeriod = 10;
 		public int muscleCellTickPeriod = 5; // also used for veins
-		public int rootCellTickPeriod = 50;
-		public int shellCellTickPeriod = 50;
+		public int rootCellTickPeriod = 100;
+		public int shellCellTickPeriod = 100;
 		public int veinCellTickPeriod = 50;
 
 		public AnimationCurve growTickPeriodAtSize; //has been stable at 80 (regardless of size) for many years,  Detatch attempt has same period as grow
 
-		public int signalTickPeriod = 20;
+		public int signalTickPeriod = 4;
 
-		public int killOldCreaturesTickPeriod = 6000;
+		public int killOldCreaturesTickPeriod = 1200;
 
 		// Terrain
-		public int portalTeleportTickPeriod = 40;
+		public int portalTeleportTickPeriod = 20;
 		public int escapistCleanupTickPeriod = 1200;
-		public int pidTickPeriod = 20;
 
 		//Panels
-		public int phenotypePanelTickPeriod = 10;
+		public int phenotypePanelTickPeriod = 4;
 	}
 	public Quality quality;
 
@@ -364,11 +365,8 @@ public class GlobalSettings : MonoSingleton<GlobalSettings> {
 
 	public Pooling pooling;
 
-	//Visual
-	//public float orthoMinStrongFX = 10f;
-	//public float orthoMaxHorizonFx = 30f;
-	public float detailedGraphicsOrthoLimit = 50f;
-	//public float loudAudioOrthoLimit = 50f;
+	// Visual and sound
+	public float detailedGraphicsOrthoLimit = 30f;
 	public AnimationCurve loudAudioVolumeAtOrtho;
 	public AnimationCurve quietAudioVolumeAtOrtho;
 
